1996f5715d6db65cb2ad9b4b28f7b0fae07f250e
Jaegyeom Kim <Jaegyeom1215@gmail.com>
2021-11-07 20:44:17 -0800

    Commit Project

---
+++ PROG123_Update/PROG123/Controllers/HomeController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using PROG123.DAL;
+using PROG123.Models;
+using PROG123.utils;
+
+namespace PROG123.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration _configuration;
+        private IConfiguration configuration;
+
+        public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            // this is for testing purpuse only.
+            DatabaseHelper dbh = new DatabaseHelper(_configuration);
+            ConnStatusModel status = dbh.GetConnectionStringAndConnectionStatus();
+            ViewBag.ConnStr = status.ConnStr;
+            ViewBag.DBStatus = status.DBConnectionStatus;
+            ViewBag.Exception = status.Exception;
+            return View();
+        }
+        public IActionResult Page2(PersonModel personModel)
+        {
+            // send the PersonModule to the DB
+            //instantiate a DALPerson object
+            DALPerson DalPerson = new DALPerson(_configuration);
+            //call the DALPerson Addperson to insert perosn module (save the person id)
+            string PersonID = DalPerson.AddPerson(personModel);
+            personModel.PersonID = PersonID;
+            //DALPerson dp = new DALPerson(configuration); 
+            // save the personID in to the session  
+
+            HttpContext.Session.SetString("PersonID", PersonID);
+            personModel.PersonID = PersonID;
+
+
+            return View(personModel);
+        }
+
+
+        public IActionResult EditPMyInfor(PersonModel pm)
+        {
+            //get the UID form the session
+            string personID = HttpContext.Session.GetString("PersonID");
+
+            //get the Person object from the DB using DALPerson class 
+            DALPerson dp = new DALPerson(_configuration);
+            PersonModel person = dp.getPerson(personID);
+            //send it to the view.
+            return View(person);
+        }
+
+        public IActionResult UpdatePersonTable(PersonModel person)
+        {
+            string personID = HttpContext.Session.GetString("PersonID");
+            person.PersonID = personID;
+            DALPerson dp = new DALPerson(_configuration);
+            dp.UpdatePerson(person);
+            return View("Page2", person);
+        }
+        public IActionResult DeletPerson()
+        {
+            //get personID form sesstion
+            string personID = HttpContext.Session.GetString("PersonID");
+
+            //instaniate and object type  DALPerson
+            DALPerson dp = new DALPerson(_configuration);
+            //call the dele method from the del person
+            dp.DeletePerson(personID);
+            return View();
+        }
+
+
+    }
+}
---
+++ PROG123_Update/PROG123/Controllers/KimController.cs
@@ -0,0 +1,134 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using PROG123.DAL;
+using PROG123.Models;
+
+namespace PROG123.Controllers
+{
+    public class KimController : Controller
+    {
+
+
+        private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration _configuration;
+        static string session;
+
+
+        public KimController(ILogger<HomeController> logger, IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult LogIn(LogInCredentialsModel logInCredentialsModel)
+        {
+            //creat a DELperson objeck
+            DALPerson DalPerson = new DALPerson(_configuration);
+            //user the DELPerson.check loginCredintials Method to check if the crdentials are vaild
+            PersonModel person = DalPerson.CheckLogInCredentials(logInCredentialsModel);
+
+            if (person == null)
+            {
+                ViewBag.LoginMessage = "Sorry. Login Fail. Try again.";
+            }
+            else
+            {
+                //PersonModel person = DalPerson.CheckLogInCredentials(logInCredentialsModel);
+                ViewBag.UserFirstName = person.FName;
+                string personID = person.PersonID;
+                session = personID;
+
+            }
+
+            return View("Index");
+        }
+
+
+        public IActionResult EnterNewProduct()
+        {
+
+            if (session == null)
+            {
+                ViewBag.LoginMessage = "User is not logged in";
+                return View("Index");
+            } 
+            else
+            {
+                return View();
+            }
+
+
+        }
+
+
+        public IActionResult AddProductToDB(ProductModel productModel)
+        {
+            if (session == null)
+            {
+                ViewBag.LoginMessage = "User is not logged in";
+                return View("Index");
+            }
+            else
+            {
+                DALProducts DalProducts = new DALProducts(_configuration);
+                string productID = DalProducts.AddNewProduc(productModel);
+                productModel.PID = productID;
+                return View("AddProductToDB", productModel);
+            }
+
+        }
+
+        public IActionResult ListAllProducts()
+        {
+            if (session == null)
+            {
+                ViewBag.LoginMessage = "User is not logged in";
+                return View("Index");
+            }
+            else
+            {                
+                DALProducts DalProducts = new DALProducts(_configuration);
+
+                LinkedList<ProductModel> list = new LinkedList<ProductModel>();
+
+                list = DalProducts.GetAllProducts();
+
+                return View(list);
+            }
+        }
+
+        public IActionResult OneClickBuy(string PID)
+        {
+            if (session == null)
+            {
+                ViewBag.LoginMessage = "User is not logged in";
+                return View("Index");
+            }
+            else
+            {
+
+                string PersonID = session;
+                DALSalesTransaction dALSalesTransaction = new DALSalesTransaction(_configuration);
+
+                //(string productID, string personID, int manyPurchasedProds)
+                dALSalesTransaction.OneClickBuy(PID, PersonID, 1);
+
+                PurchaseModel purchaseModel = dALSalesTransaction.OneClickBuy(PID, PersonID, 1);
+
+                return View(purchaseModel);
+            }
+        }
+
+    }
+}
